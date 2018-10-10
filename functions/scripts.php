3a5ae574c8bf14f9897c551acde0cf3218dffb98
Md. Mostak Shahid <mostak.shaid@gmail.com>
2018-10-10 12:45:54 +0600

    1.0.0

--- functions/scripts.php
+++ functions/scripts.php
@@ -44,6 +44,8 @@ function mosacademy_enqueue_scripts() {
 	wp_register_script('slimscroll', get_template_directory_uri() . '/plugins/slimscroll/jquery.slimscroll.js', 'jquery');
 	wp_register_script('wow.min', get_template_directory_uri() . '/plugins/wow/wow.min.js', 'jquery');
 	
+	wp_register_script('jquery.lazy.min', get_template_directory_uri() . '/plugins/jquery.lazy-master/jquery.lazy.min.js', 'jquery');
+	
 	wp_register_script('main', get_template_directory_uri() . '/js/main.js', 'jquery');
 
 	wp_enqueue_script( 'jquery' );
@@ -55,6 +57,7 @@ function mosacademy_enqueue_scripts() {
 	wp_enqueue_script( 'jquery.sticky' );
 	wp_enqueue_script( 'slimscroll' );
 	wp_enqueue_script( 'wow.min' );
+	wp_enqueue_script( 'jquery.lazy.min' );
 	wp_enqueue_script( 'main' );
 
 }
