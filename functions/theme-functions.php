c569459a0e4cc6cd5beabb3766c0a7a4c068b4f0
Md. Mostak Shahid <mostak.shahid@gmail.com>
2018-11-08 00:19:40 +0600

    1.0.0

--- functions/theme-functions.php
+++ functions/theme-functions.php
@@ -374,3 +374,17 @@ function mos_get_terms ($taxonomy = 'category') {
     return $output;
 }
 //$terms = mos_get_terms ("testimonial-category");
+
+
+/**
+ * Defer parsing of javascript.
+ */
+if (!(is_admin() )) {
+    function defer_parsing_of_js ( $url ) {
+        if ( FALSE === strpos( $url, '.js' ) ) return $url;
+        if ( strpos( $url, 'jquery.js' ) ) return $url;
+        // return "$url' defer ";
+        return "$url' defer onload='";
+    }
+    add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
+}
